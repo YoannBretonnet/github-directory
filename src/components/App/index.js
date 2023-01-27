cbbadb2eb8af6c3e73e087b31150835bc153ef0c
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-01-27 15:42:31 +0100

    feat: searpage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 
 import Header from '../Header';
+import SearchPage from '../pages/SearchPage';
+import FaqPage from '../pages/FaqPage';
 
-import './app.css';
+import './app.scss';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+      </Routes>
     </div>
   );
 }
